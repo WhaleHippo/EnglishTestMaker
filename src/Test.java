ec615c5f48149d8ae847c7849f44216afb298400
whalehippo <dany950319@gmail.com>
2015-10-05 12:04:10 +0900

    엑셀파일을 열어서 dropbox에 비스킷 폼을 만족시키도록 변환

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -14,41 +15,58 @@ public class Test {
 	 */
 	public static void main(String[] args) throws IOException {
 		FileOutputStream fileOut = null;
+		FileWriter convertText = new FileWriter("/home/whalehippo/Dropbox/앱/Biscuit/english text.txt");
 		try {
-			XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream("C:/Users/Administrator/Desktop/hippo.xlsx"));
+			XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream("/home/whalehippo/hippo.xlsx"));
 
 			System.out.println("시트개수 : " + wb.getNumberOfSheets());
 
 			XSSFSheet sheet = wb.getSheetAt(0);
 			int rows = sheet.getPhysicalNumberOfRows();// 몇줄 인지
 			System.out.println("행수 : " + rows);
-			for (int r = 0; r < rows; r++) {
+			for (int r = 0; r < 50; r++) {
 				XSSFRow row = sheet.getRow(r);// 한줄 얻어오기
 				if (row == null) // 못읽어오면 다음줄로
 				{
 					continue;
 				}
 				int cells = row.getPhysicalNumberOfCells();// 몇칸인지
-				System.out.println(cells);
+				System.out.println("행수 : "+cells);
 				int result = 0;
-				for (int c = 0; c < cells; c++)// 칸수 만큼 작업
+				String value="";
+				/*for (int c = 0; c < 2; c++)// 칸수 만큼 작업
 				{
 					XSSFCell cell = row.getCell(c);// 한칸 얻어오기
-					String value = cell.getStringCellValue(); // 셀의 데이터를 string으로 가져오기
 
-					System.out.print(value + "\t");// 읽어온 칸의 값을 출력
-				}
+					if(cell.getCellType()==XSSFCell.CELL_TYPE_NUMERIC) // 타입 검사. 안하면 에러남
+					{
+						value = value + cell.getNumericCellValue()+"\t";
+					}
+					else if(cell.getCellType()==XSSFCell.CELL_TYPE_STRING)
+					{
+						value = value + cell.getStringCellValue()+"\t"; // 셀의 데이터를 string으로 가져오기
+					}
+				}*/
+				
+				XSSFCell cell = row.getCell(0);// 한칸 얻어오기
+				value = value + cell.getStringCellValue()+"\t"+" "+" ";
+				cell = row.getCell(1);// 한칸 얻어오기
+				value = value + cell.getStringCellValue()+"\t";
+				value = value + "0" +"\n";
+				System.out.print(r+":"+value);
+				convertText.write(value);
+				value="";
 
 				row.createCell(5).setCellValue(result);// 그 행의 index가 5인 셀이 result의 밸류를 넣는다.
 
-				System.out.println();// 줄바꿈
 			}
 
-			fileOut = new FileOutputStream("C:/Users/Administrator/Desktop/test.xlsx");
+			fileOut = new FileOutputStream("/home/whalehippo/hippoprime.xlsx");
 			wb.write(fileOut);// 파일에 저장
 			wb.close();
 		} finally {
 			fileOut.close();
+			convertText.close();
 			System.out.println("종료");
 		}
 	}
